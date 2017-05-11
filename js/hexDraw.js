3f51b4a903f0e2f3a162197e99f011d9a7e6333a
GreenCillah <thecoolgreenman@gmail.com>
2017-05-11 22:23:33 +1000

    Added Zoom in and out. Increased hex number lots. seperated HTML and Script

--- js/hexDraw.js
+++ js/hexDraw.js
@@ -1,5 +1,5 @@
-var width = 1960,
-    height = 1500,
+var width = 10000,
+    height = 10000,
     radius = 30;
 
 var topology = hexTopology(radius, width, height);
@@ -7,100 +7,140 @@ var topology = hexTopology(radius, width, height);
 var projection = hexProjection(radius);
 
 var path = d3.geo.path()
-    .projection(projection);
+    .projection(projection);;
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-svg.append("g")
-    .attr("class", "hexagon")
-  .selectAll("path")
-    .data(topology.objects.hexagons.geometries)
-  .enter().append("path")
-    .attr("d", function(d) { return path(topojson.feature(topology, d)); })
-    .attr("class", function(d) { return d.fill ? "fill" : null; })
-    .on("mousedown", mousedown)
-    .on("mousemove", mousemove)
-    .on("mouseup", mouseup);
-
-svg.append("path")
-    .datum(topojson.mesh(topology, topology.objects.hexagons))
-    .attr("class", "mesh")
-    .attr("d", path);
-
-var border = svg.append("path")
-    .attr("class", "border")
-    .call(redraw);
-
+var svg;
+var border;
 var mousing = 0;
-	
 
 function mousedown(d) {
-  mousing = d.fill ? -1 : +1;
-  mousemove.apply(this, arguments);
+    mousing = d.fill ? -1 : +1;
+    mousemove.apply(this, arguments);
+
 }
 
 function mousemove(d) {
-  if (mousing) {
-    d3.select(this).classed("fill", d.fill = mousing > 0);
-    border.call(redraw);
-  }
+    if (mousing) {
+        d3.select(this).classed("fill", d.fill = mousing > 0);
+        border.call(redraw);
+    }
 }
 
 function mouseup() {
-  mousemove.apply(this, arguments);
-  mousing = 0;
+    mousemove.apply(this, arguments);
+    mousing = 0;
 }
 
 function redraw(border) {
-  border.attr("d", path(topojson.mesh(topology, topology.objects.hexagons, function(a, b) { return a.fill ^ b.fill; })));
+    border.attr("d", path(topojson.mesh(topology, topology.objects.hexagons, function (a, b) {
+        return a.fill ^ b.fill;
+    })));
+}
+
+function zoom() {
+    svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
 
 function hexTopology(radius, width, height) {
-  var dx = radius * 2 * Math.sin(Math.PI / 3),
-      dy = radius * 1.5,
-      m = Math.ceil((height + radius) / dy) + 1,
-      n = Math.ceil(width / dx) + 1,
-      geometries = [],
-      arcs = [];
+    var dx = radius * 2 * Math.sin(Math.PI / 3),
+        dy = radius * 1.5,
+        m = Math.ceil((height + radius) / dy) + 1,
+        n = Math.ceil(width / dx) + 1,
+        geometries = [],
+        arcs = [];
 
-  for (var j = -1; j <= m; ++j) {
-    for (var i = -1; i <= n; ++i) {
-      var y = j * 2, x = (i + (j & 1) / 2) * 2;
-      arcs.push([[x, y - 1], [1, 1]], [[x + 1, y], [0, 1]], [[x + 1, y + 1], [-1, 1]]);
+    for (var j = -1; j <= m; ++j) {
+        for (var i = -1; i <= n; ++i) {
+            var y = j * 2,
+                x = (i + (j & 1) / 2) * 2;
+            arcs.push([[x, y - 1], [1, 1]], [[x + 1, y], [0, 1]], [[x + 1, y + 1], [-1, 1]]);
+        }
     }
-  }
 
-  for (var j = 0, q = 3; j < m; ++j, q += 6) {
-    for (var i = 0; i < n; ++i, q += 3) {
-      geometries.push({
-        type: "Polygon",
-        arcs: [[q, q + 1, q + 2, ~(q + (n + 2 - (j & 1)) * 3), ~(q - 2), ~(q - (n + 2 + (j & 1)) * 3 + 2)]],
-        fill: Math.random() > i / n * 2
-      });
+    for (var j = 0, q = 3; j < m; ++j, q += 6) {
+        for (var i = 0; i < n; ++i, q += 3) {
+            geometries.push({
+                type: "Polygon",
+                arcs: [[q, q + 1, q + 2, ~(q + (n + 2 - (j & 1)) * 3), ~(q - 2), ~(q - (n + 2 + (j & 1)) * 3 + 2)]],
+                fill: Math.random() > i / n * 2
+            });
+        }
     }
-  }
 
-  return {
-    transform: {translate: [0, 0], scale: [1, 1]},
-    objects: {hexagons: {type: "GeometryCollection", geometries: geometries}},
-    arcs: arcs
-  };
+    return {
+        transform: {
+            translate: [0, 0],
+            scale: [1, 1]
+        },
+        objects: {
+            hexagons: {
+                type: "GeometryCollection",
+                geometries: geometries
+            }
+        },
+        arcs: arcs
+    };
 }
 
 function hexProjection(radius) {
-  var dx = radius * 2 * Math.sin(Math.PI / 3),
-      dy = radius * 1.5;
-  return {
-    stream: function(stream) {
-      return {
-        point: function(x, y) { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
-        lineStart: function() { stream.lineStart(); },
-        lineEnd: function() { stream.lineEnd(); },
-        polygonStart: function() { stream.polygonStart(); },
-        polygonEnd: function() { stream.polygonEnd(); }
-      };
-    }
-  };
+    var dx = radius * 2 * Math.sin(Math.PI / 3),
+        dy = radius * 1.5;
+    return {
+        stream: function (stream) {
+            return {
+                point: function (x, y) {
+                    stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2);
+                },
+                lineStart: function () {
+                    stream.lineStart();
+                },
+                lineEnd: function () {
+                    stream.lineEnd();
+                },
+                polygonStart: function () {
+                    stream.polygonStart();
+                },
+                polygonEnd: function () {
+                    stream.polygonEnd();
+                }
+            };
+        }
+    };
+}
+
+function fnStart() {
+
+    svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .call(d3.behavior.zoom().scaleExtent([.01, 50]).on("zoom", zoom))
+        .append("g");
+
+    border = svg.append("path")
+        .attr("class", "border")
+        .call(redraw);
+
+    svg.append("g")
+        .attr("class", "hexagon")
+        .selectAll("path")
+        .data(topology.objects.hexagons.geometries)
+        .enter().append("path")
+        .attr("d", function (d) {
+            return path(topojson.feature(topology, d));
+        })
+        .attr("class", function (d) {
+            return d.fill ? "fill" : null;
+        })
+        .on("mousedown", mousedown)
+        .on("mousemove", mousemove)
+        .on("mouseup", mouseup);
+
+    svg.append("path")
+        .datum(topojson.mesh(topology, topology.objects.hexagons))
+        .attr("class", "mesh")
+        .attr("d", path);
+
+
+
 }
